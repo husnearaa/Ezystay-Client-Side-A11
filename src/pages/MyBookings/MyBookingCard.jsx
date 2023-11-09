8a4af53e454beb6aed8ace321e5873ae22f0c755
husneara <husnearaa99@gmail.com>
2023-11-09 23:39:23 +0600

    has added some feature

--- src/pages/MyBookings/MyBookingCard.jsx
+++ src/pages/MyBookings/MyBookingCard.jsx
@@ -1,8 +1,13 @@
 
 
-const MyBookingCard = ({booking}) => {
+const MyBookingCard = ({booking, handleDelete, handleBookingUpdate}) => {
 
-    const { title, image, size, bookingRoom, price_per_night,} = booking;
+    const { _id, title, image, size, bookingRoom, price_per_night, status} = booking;
+
+   
+
+
+
 
     return (
         <div>
@@ -14,13 +19,13 @@ const MyBookingCard = ({booking}) => {
                             <div className="ml-5">
                                 <h2 className="card-title pb-4">{title}</h2>
                                 <p className="pb-1">Price Per Night: ${price_per_night}</p>
-                                <p className="mb-3">{bookingRoom}</p>
+                                {/* <p className="mb-3">{bookingRoom}</p> */}
                                 <button className="btn text-white bg-blue-500">your review</button>
                             </div>
                             <div className="card-actions rounded items-center justify-end">
                                 <div className="btn-group btn-group-vertical space-y-4">
-                                    <button className="btn text-white bg-green-400">update date</button>
-                                    <button className="btn text-white  bg-orange-500">cancel booking</button>
+                                    <button onClick={() => handleBookingUpdate(_id)} className="btn text-white bg-green-400">update date</button>
+                                    <button onClick={() => handleDelete(_id)} className="btn text-white  bg-orange-500">cancel booking</button>
                                 </div>
                             </div>
                         </div>
--- src/pages/MyBookings/MyBookings.jsx
+++ src/pages/MyBookings/MyBookings.jsx
@@ -1,22 +1,82 @@
 
 import { useContext, useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
-import {AuthContext} from '../../providers/AuthProvider';
+import { AuthContext } from '../../providers/AuthProvider';
 import MyBookingCard from "./MyBookingCard";
+import Swal from "sweetalert2";
 
 
 const MyBookings = () => {
 
-  const {user} = useContext(AuthContext);
-    const [bookings, setBookings] =useState([]);
+    const { user } = useContext(AuthContext);
+    const [bookings, setBookings] = useState([]);
 
-  const url = `http://localhost:5000/bookings?email=${user?.email}`
 
-  useEffect(() => {
-    fetch(url)
-    .then(res => res.json())
-    .then(data => setBookings(data))
-  }, [url])
+    const url = `http://localhost:5000/bookings?email=${user?.email}`
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setBookings(data))
+    }, [url])
+
+
+
+
+    const handleDelete = (id) => {
+        console.log(id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/bookings/${id}`, {
+                    method: "DELETE"
+                })
+                    .then((res) => {
+                        console.log(res);
+                        Swal.fire("Successfully Deleted!", "Successfully deleted from the cart", "success")
+                        const remaining = bookings.filter(booking => booking._id !== id);
+                        setBookings(remaining);
+
+                      
+                    }
+                    )
+                    .catch(error => console.log(error))
+            }
+        })
+    }
+
+
+
+    const handleBookingUpdate = id => {
+        fetch(`https://localhost:5000/bookings/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status: 'update' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    // update state
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    const updated = bookings.find(booking => booking._id === id);
+                    updated.status = 'update'
+                    const newBookings = [updated, ...remaining];
+                    setBookings(newBookings);
+                }
+            })
+    }
+
+
 
     return (
         <div>
@@ -29,12 +89,14 @@ const MyBookings = () => {
             {/* <h2>booking: {bookings.length}</h2> */}
             <h2 className="text-5xl  text-center py-10">My Bookings</h2>
 
-           {
-            bookings.map(booking => <MyBookingCard
-            key={booking._id}
-            booking={booking}
-            ></MyBookingCard>)
-           }
+            {
+                bookings.map(booking => <MyBookingCard
+                    key={booking._id}
+                    booking={booking}
+                    handleDelete={handleDelete}
+                    handleBookingUpdate={handleBookingUpdate}
+                ></MyBookingCard>)
+            }
 
         </div>
     );
